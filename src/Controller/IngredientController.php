e36f5983d634b11534ea99c57bfd98eb97bb06f5
Emmanuel <e.amargier@gmail.com>
2023-12-20 09:39:43 +0100

    Add modal to create an ingredient and add pagination for the list of the them

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,17 +3,31 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
     #[Route('/ingredient', name: 'ingredient')]
-    public function index(IngredientRepository $ingredientRepository): Response
+    public function index(IngredientRepository $ingredientRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $ingredients = $paginator->paginate(
+            $ingredientRepository->findAll(), 
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
         return $this->render('pages/ingredient/index.html.twig', [
-            'ingredients' => $ingredientRepository->findAll()
+            'ingredients' => $ingredients
         ]);
     }
+
+    // public function create(Request $request, EntityManagerInterface $entity_manager): Response
+    // {
+
+    // }
 }
